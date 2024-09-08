6736d7c404ecb08f17cc82515ac88481c5334560
Niku <nokka200@gmail.com>
2024-09-08 10:59:13 +0300

    6.5. valmis

--- src/App.jsx
+++ src/App.jsx
@@ -44,8 +44,11 @@ const App = () => {
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
+      {anecdotes
+        .slice() // docs: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
+        .sort((a, b) => b.votes - a.votes)
+        .map(anecdote =>
+          <div key={anecdote.id}>
           <div>
             {anecdote.content}
           </div>
