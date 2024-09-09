12466151867108e3cd4440a1c529f932ec2bf626
Niku <nokka200@gmail.com>
2024-09-09 17:33:10 +0300

    6.8. valmis tiedosto puuttui edellisestä

---
+++ src/components/AnecdoteList.jsx
@@ -0,0 +1,34 @@
+import { useSelector, useDispatch } from 'react-redux'
+import { increaseVote } from '../reducers/anecdoteReducer'
+
+const AnecdoteList = () => {
+  const anecdotes = useSelector(state => state)
+  const dispatch = useDispatch()
+
+  const vote = (id) => {
+    console.log('vote', id)
+    dispatch(increaseVote(id))
+  }
+
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      {anecdotes
+        .slice() // docs: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
+        .sort((a, b) => b.votes - a.votes)
+        .map(anecdote =>
+          <div key={anecdote.id}>
+            <div>
+              {anecdote.content}
+            </div>
+            <div>
+              has {anecdote.votes}
+              <button onClick={() => vote(anecdote.id)}>vote</button>
+            </div>
+          </div>
+        )}
+    </div>
+  );
+};
+
+export default AnecdoteList;
